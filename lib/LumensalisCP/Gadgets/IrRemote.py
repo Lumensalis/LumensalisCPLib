e31b68ffa9e433f144db11011a488d4f3e4263a7
James Fowler <james@openseaconsulting.com>
2025-07-14 09:39:08 -0400

    IR fixes / updates

--- lib/LumensalisCP/Gadgets/IrRemote.py
+++ lib/LumensalisCP/Gadgets/IrRemote.py
@@ -21,12 +21,40 @@ class LCP_IRrecv(MainChild):
             "PLAY": 0xffc23d,
             "VOL-": 0xffe01f,
             "VOL+": 0xffa857,
+        },
+        "dvd_remote": {
+            "DOWN":0x59a66a95,
+            "UP" : 0x59a6f20d,
+            "PREV": 0x59a6609f,
+            "NEXT": 0x59a640bf,
+            "PLAY": 0x59a6c03f,
+            "STOP": 0x59a6f00f,
+            "PAUSE": 0x59a6d827,
+            "MENU": 0x59a6fa05,
+            "0": 0x59a60af5,
+            "1": 0x59a65aa5,
+            "2": 0x59a67a85,
+            "3": 0x59a6728d,
+            "4": 0x59a6ba45,
+            "5": 0x59a68a75,
+            "6": 0x59a6926d,
+            "7": 0x59a69a65,
+            "8": 0x59a6aa55,
+            "9": 0x59a6b24d,
+            
         }
+        
+         # 0xacd3354a
     }
     
-    def __init__(self, pin, codenames:Mapping = {}, main:MainManager = None, name:str|None = None, updateInterval = 0.1 ):
+    showUnhandled:bool
+    
+    def __init__(self, pin, codenames:Mapping = {}, main:MainManager = None, 
+                 name:str|None = None, updateInterval = 0.1,
+                 showUnhandled = False ):
         super().__init__(name=name or LCP_IRrecv.__name__, main=main )
-        maxLen = 29
+        maxLen = 128
+        self.showUnhandled = showUnhandled 
         try:
             self.pulseIn = pulseio.PulseIn(pin, maxlen=maxLen, idle_state=True)
         except Exception as inst:
@@ -64,6 +92,7 @@ class LCP_IRrecv(MainChild):
             
         main.callLater( startIrTimer )
 
+
     @property
     def jsonCatalog(self) -> Mapping[str,int]:
         if self.__jsonCatalog is None:
@@ -78,13 +107,15 @@ class LCP_IRrecv(MainChild):
     
     def handleRawCode(self, rawCode ):
         code = 0
+        self.enableDbgOut and self.dbgOut( f"handleRawCode {rawCode}" )
         for byte in rawCode:
             code = (code *256) + byte
             
-            
+        
+        
         cb = self.__callbacksByCode.get(code,None) # self.__unhandledCallback)
         if cb is not None:
-            self.enableDbgOut and self.dbgOut( f"calling callback for code {'x'%code}, cb={cb}" )
+            self.enableDbgOut and self.dbgOut( f"calling callback for code {code}, cb={cb}" )
             cb()
         else:
             self._unhandled(code, rawCode)
@@ -96,8 +127,10 @@ class LCP_IRrecv(MainChild):
                 self.__unhandledCallback( code=code, rawCode=rawCode)
             except Exception as inst:
                 self.SHOW_EXCEPTION( inst, "unhandledCallback failed for %x from %r", code, rawCode )
-        else:
-            self.enableDbgOut and self.dbgOut( f"unhandled remote code: 0x{'%x'%code} from {rawCode}" )
+        #else:
+        #    self.enableDbgOut and self.dbgOut( f"unhandled remote code: 0x{'%x'%code} from {rawCode}" )
+        if self.showUnhandled: 
+            self.infoOut( f"unhandled remote code: 0x{'%x'%code} from {rawCode}" )
 
     def setUnhandledCallback( self, cb:Callable ):
         self.__unhandledCallback = cb
@@ -141,7 +174,7 @@ class LCP_IRrecv(MainChild):
             adafruit_irremote.IRDecodeException,
             adafruit_irremote.FailedToDecode,
         ) as e:  # failed to decode
-            self.enableDbgOut and self.dbgOut("Failed to decode: %s", e.args)
+            self.enableDbgOut and self.dbgOut("Failed to decode: %r /  %s %r", pulses, e.args, e )
 
     
 def onIRCode( ir: LCP_IRrecv, code:int|str ):
