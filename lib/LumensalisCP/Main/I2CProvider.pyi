2f257bea61c2ff6d36dfede472e8d115bcb38e1a
James Fowler <james@openseaconsulting.com>
2025-07-15 14:29:01 -0400

    add .pyi for I2CProvider

---
+++ lib/LumensalisCP/Main/I2CProvider.pyi
@@ -0,0 +1,34 @@
+
+from LumensalisCP.Identity.Local import NamedLocalIdentifiableList
+from LumensalisCP.common import *
+from LumensalisCP.CPTyping import *
+
+import busio
+import board
+from LumensalisCP.I2C.I2CDevice import I2CDevice
+import LumensalisCP.I2C.I2CFactory
+import LumensalisCP.I2C.Adafruit.AdafruitI2CFactory
+
+class I2CProvider(object):
+    def __init__(self,config = None, main=None): pass
+        
+        
+    _i2cDevicesContainer: NamedLocalIdentifiableList
+    adafruitFactory : LumensalisCP.I2C.Adafruit.AdafruitI2CFactory.AdafruitFactory
+    i2cFactory: LumensalisCP.I2C.I2CFactory.I2CFactory
+    __i2cChannels:Mapping[Tuple[int,int],busio.I2C]
+    __i2cDevices:List["LumensalisCP.I2C.I2CDevice.I2CDevice"]
+    __defaultI2C:busio.I2C
+    __identityI2C:Any
+
+
+    defaultI2C:busio.I2C
+            
+    def _addI2CDevice(self, target:I2CDevice ): ...
+    
+    def _addBoardI2C( self, board, i2c:busio.I2C ): ...
+    
+    @property
+    def identityI2C(self): return self.__identityI2C
+
+    def addI2C(self, sdaPin, sclPin) -> busio.I2C: ...
