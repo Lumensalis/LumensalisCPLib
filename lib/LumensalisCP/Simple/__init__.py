7df654d8981c1acc3bf7d68634f1f6ae0d4b3e06
James Fowler <james@openseaconsulting.com>
2025-07-16 15:08:43 -0400

    fireable / do / doDbg

--- lib/LumensalisCP/Simple/__init__.py
+++ lib/LumensalisCP/Simple/__init__.py
@@ -6,9 +6,17 @@ Intended to be used as `from LumensalisCP.Simple import *`
 from TerrainTronics.Demos.DemoCommon import *
 from LumensalisCP.Lights.Values import RGB
 from LumensalisCP.Main.Manager import MainManager
+from LumensalisCP.Triggers.Fireable import Fireable
 
 def ProjectManager( ) -> MainManager:
     return  MainManager.initOrGetManager()
 
 def getColor( v ) -> RGB:
     return LightValueRGB.toRGB(v)
+
+
+def do( cb, *args, **kwds ):
+    return Fireable.makeCallback( cb, *args, **kwds )
+
+def doDbg( cb, *args, **kwds ):
+    return Fireable.makeDbgCallback( cb, *args, **kwds )
---
+++ lib/LumensalisCP/Triggers/Fireable.py
@@ -0,0 +1,76 @@
+from LumensalisCP.Debug import Debuggable
+from LumensalisCP.IOContext import *
+from LumensalisCP.Main.Updates import evaluate
+from LumensalisCP.util import kwCallback
+
+if TYPE_CHECKING:
+    FireableCBArg = Union[Callable,'Fireable',KWCallback]
+    FireCondition = Union[bool,Callable,Evaluatable]
+
+class Fireable( Debuggable ):
+
+    @classmethod
+    def makeCallback( cls, cb:FireableCBArg, *args, **kwds ):
+        return FireableCB( cb, *args, **kwds )
+        
+    @classmethod
+    def makeDbgCallback( cls, cb:FireableCBArg, *args, **kwds ):
+        rv = FireableCB( cb, *args, **kwds )
+        rv.enableDbgOut = True
+        return rv
+
+    def __init__(self):
+        super().__init__()
+    
+    def fire(self, context:UpdateContext) -> Any:
+        raise NotImplemented
+    
+    def unless( self, condition:FireCondition):
+        return FireableUnless( condition, self )
+    
+    def __call__(self, context:Optional[UpdateContext]=None) ->Any :
+        return self.fire(context=UpdateContext.fetchCurrentContext(context))
+        
+
+class FireableCB( Fireable ):
+
+    def __init__( self, cb:FireableCBArg, *args, **kwds ):
+        super().__init__()
+        self._args = args
+        self._kwds = kwds
+        self._cb = KWCallback( cb )
+    
+    def fire(self, context:UpdateContext) ->Any :
+        
+        if self.enableDbgOut or context.debugEvaluate: 
+            with context.nestDebugEvaluate() as nde:
+                nde.say( self, "firing" )
+                rv = self._cb( *self._args, context=context,**self._kwds )
+                return rv
+        else:
+            return self._cb( *self._args, context=context,**self._kwds )
+    
+class FireableUnless( Fireable ):
+
+    def __init__( self, condition:FireCondition, action:Fireable ):
+        super().__init__()
+        self._condition = condition
+        self._action =  action
+        self.enableDbgOut = action.enableDbgOut
+        
+    def fire(self, context:UpdateContext) ->Any :
+        if self.enableDbgOut or context.debugEvaluate: 
+            with context.nestDebugEvaluate() as nde:
+                
+                unless = evaluate(self._condition,context )
+                if not unless:
+                    nde.say( self, "unless firing..." )
+                    return self._action.fire( context )
+                else:
+                    nde.say( self, "unless skipped" )
+                    
+        else:
+            unless = evaluate(self._condition,context )
+            if not unless:
+                return self._action.fire( context )
+        
