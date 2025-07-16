9b20421d4de392d149c64e443b0ecfb5757183bd
James Fowler <james@openseaconsulting.com>
2025-07-16 15:09:34 -0400

    updateValue debugEvaluate

--- lib/LumensalisCP/Inputs.py
+++ lib/LumensalisCP/Inputs.py
@@ -1,43 +1,32 @@
 from LumensalisCP.Main.Expressions import *
 from LumensalisCP.Identity.Local import NamedLocalIdentifiable
+from LumensalisCP.common import safeFmt
 
+if TYPE_CHECKING:
+    pass
 #############################################################################
 
-class InputSource(NamedLocalIdentifiable, ExpressionTerm, Debuggable):
+class InputSource(NamedLocalIdentifiable, ExpressionTerm):
 
-    def __init__(self, name:str = None):
+    def __init__(self, name:Optional[str] = None):
         NamedLocalIdentifiable.__init__(self,name=name)
         ExpressionTerm.__init__(self)
-        Debuggable.__init__(self)
-
+        
         self.__latestValue = None
-        self.__latestUpdateIndex:int = None
-        self.__latestChangeIndex:int = None
+        self.__latestUpdateIndex:int = -1
+        self.__latestChangeIndex:int = -1
         self.__onChangedList = []
 
     def __repr__( self ):
         return safeFmt( "%s:%s = %r", self.__class__.__name__, self.name, self.value )
     
-    def getDerivedValue(self, context:UpdateContext) -> Any:
+    def getDerivedValue(self, context:EvaluationContext) -> Any:
         raise NotImplemented
     
     def onChange(self, cb:Callable):
         self.__onChangedList.append(cb)
 
-    def updateValue(self, context:UpdateContext) -> bool:
-        assert isinstance( context, UpdateContext )
-        if self.__latestChangeIndex == context.updateIndex:
-            context.addChangedSource( self )
-            return self.__latestValue
-        
-        val = self.getDerivedValue( context )
-        self.__latestUpdateIndex = context.updateIndex
-        if val == self.__latestValue:
-            return False
-        
-        self.enableDbgOut and self.dbgOut( f"value changing on {self.name} from {self.__latestValue} to {val} on update {context.updateIndex}" )
-        self.__latestValue = val
-        self.__latestChangeIndex = context.updateIndex
+    def __callOnChanged(self, context:EvaluationContext):
         context.addChangedSource( self )
         for cb in self.__onChangedList:
             try:
@@ -45,11 +34,36 @@ class InputSource(NamedLocalIdentifiable, ExpressionTerm, Debuggable):
             except Exception as inst:
                 self.SHOW_EXCEPTION( inst, "onChanged callback %r failed", cb )
 
+    def updateValue(self, context:EvaluationContext) -> bool:
+        assert isinstance( context, EvaluationContext )
+        if self.__latestChangeIndex == context.updateIndex:
+            context.addChangedSource( self )
+            return True
+
+        if context.debugEvaluate or self.enableDbgOut:
+            with context.nestDebugEvaluate() as nde:
+                val = self.getDerivedValue( context )
+                self.__latestUpdateIndex = context.updateIndex
+                
+                if val == self.__latestValue:
+                    nde.say( self, "updateValue unchanged (%r) on update %d", self.__latestValue, context.updateIndex )
+                    return False
+                else:
+                    nde.say( self, "value changed from %r to %r on %s", self.__latestValue, val, context.updateIndex )
+        else:
+            val = self.getDerivedValue( context )
+            self.__latestUpdateIndex = context.updateIndex
+            if val == self.__latestValue:
+                return False
+                
+        self.__latestValue = val
+        self.__latestChangeIndex = context.updateIndex
+
         return True
 
     
-    def cyclesSinceChange(self,context:UpdateContext) -> int:
-        return UpdateContext.fetchCurrentContext(context)
+    def cyclesSinceChange(self,context:EvaluationContext) -> int:
+        return UpdateContext.fetchCurrentContext(context).updateIndex - self.__latestChangeIndex
                 
     @property
     def value(self): return self.__latestValue
@@ -58,11 +72,11 @@ class InputSource(NamedLocalIdentifiable, ExpressionTerm, Debuggable):
         return bool(self.__latestValue)
     
     @override
-    def getValue(self, context:EvaluationContext = None ) -> Any:
+    def getValue(self, context:Optional[EvaluationContext] = None ) -> Any:
         if context is not None and self.__latestUpdateIndex != context.updateIndex:
             self.updateValue( context )
         return self.__latestValue
     
     def path( self ): return None
 
-__all__ = [ InputSource ]
+__all__ = [ 'InputSource' ]
